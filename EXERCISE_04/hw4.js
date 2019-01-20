70c7d491c1c266b0379915bc7f5ac977fce728e1
DESKTOP-P77TBHV\ליאור <lior@bllush.com>
2019-01-20 03:41:29 +0200

    EXERCISE_06

--- EXERCISE_04/hw4.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-let server = http.createServer(
-							function(request, response) {
-								response.writeHead(200);
-								console.log(request.headers.host+request.url);
-								response.end(request.headers.host+request.url);
-							});									
-server.listen(8080);
-
